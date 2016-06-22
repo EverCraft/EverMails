65795743606c713aa8a39b046a80075eb209acdf
rexbut <butbutdu45@gmail.com>
2016-06-22 07:21:57 +0200

    Update : Commande
    Signed-off-by: rexbut <butbutdu45@gmail.com>

--- src/main/java/fr/evercraft/evermails/EMCommand.java
+++ src/main/java/fr/evercraft/evermails/EMCommand.java
@@ -29,8 +29,8 @@ import org.spongepowered.api.text.action.TextActions;
 import org.spongepowered.api.text.format.TextColors;
 
 import fr.evercraft.everapi.EAMessage.EAMessages;
-import fr.evercraft.everapi.command.ECommand;
 import fr.evercraft.everapi.plugin.EChat;
+import fr.evercraft.everapi.plugin.command.ECommand;
 import fr.evercraft.everapi.services.pagination.CommandPagination;
 import fr.evercraft.evermails.EMMessage.EMMessages;
 
